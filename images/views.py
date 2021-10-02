b7c2dea4404a02ce8016e7ae073e6cc1f96e314a
borodedamie <opeoluborode@yahoo.com>
2021-10-02 09:51:30 -0700

    implemented pytesseract to extract text from images

--- images/views.py
+++ images/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Image
 from .forms import ImageForm
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import os
+import glob
+import cv2
+import pytesseract
 
 # Create your views here.
 def index(request):
@@ -15,3 +19,29 @@ def index(request):
     context = {'form': form}
     return render(request, 'images/index.html', context)
 
+def toText(request):
+    # get media directory using os
+    path_for_license_plates = os.getcwd() + "/media/images/**/*.jpg"
+    list_license_plates = []
+    predicted_license_plates = []
+    
+    for path_to_license_plate in glob.glob(path_for_license_plates, recursive=True):
+        
+        # get the first file name including .jpg
+        license_plate_file = path_to_license_plate.split("/")[-1]
+        #splits the image name and .jpg file extension into two separate entities license_plate, _
+        license_plate, _ = os.path.splitext(license_plate_file)
+        
+        list_license_plates.append(license_plate)
+        
+        img = cv2.imread(path_to_license_plate)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'        
+        predicted_result = pytesseract.image_to_string(img_rgb, lang='eng',config='--oem 3 --psm 6')
+        # -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+        
+        filter_predicted_result = "".join(predicted_result.split()).replace(":", "").replace("-", "")
+        predicted_license_plates.append(filter_predicted_result)
+        
+        return HttpResponse(predicted_license_plates)
--- myfirstdjangosite/urls.py
+++ myfirstdjangosite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('images', include('images.urls'))
+    path('images/', include('images.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
