b3fc51b7364e1f7552f4039cbe93ba5d3ec07cbc
borodedamie <opeoluborode@yahoo.com>
2021-10-08 10:00:53 -0700

    moved images app url to admin.urls

--- myfirstdjangosite/urls.py
+++ myfirstdjangosite/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from images import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('images/', include('images.urls'))
+    # path('images/', include('images.urls'))
+    path('', views.index, name='index'),
+    path('text/', views.toText, name='text')
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
